0324f766110de322f22f7b778457119302d06808
Aman Personel <aman@Amans-MacBook-Air.local>
2024-10-20 16:46:03 +0530

    card size fix

--- src/App.js
+++ src/App.js
@@ -5,22 +5,22 @@ function App() {
   return (
     <div className="App">
       <div className='card'>
-        <div style={{width:'100%',height:'40%'}}><SkeletonLoader /> </div>
-        <div style={{width:'100%',height:'60%',padding:'4px'}}>
+        <div style={{width:'100%',height:'50%'}}><SkeletonLoader /> </div>
+        <div style={{width:'100%',height:'50%',padding:'4px'}}>
           <div style={{margin:'10px 4px',width:'90%',height:'15%'}}><SkeletonLoader /></div>
           <div style={{margin:'10px 4px',width:'90%',height:'35%'}}><SkeletonLoader /></div>
         </div>
       </div>
       <div className='card'>
-        <div style={{width:'100%',height:'40%'}}><SkeletonLoader /> </div>
-        <div style={{width:'100%',height:'60%',padding:'4px'}}>
+        <div style={{width:'100%',height:'50%'}}><SkeletonLoader /> </div>
+        <div style={{width:'100%',height:'50%',padding:'4px'}}>
           <div style={{margin:'10px 4px',width:'90%',height:'15%'}}><SkeletonLoader /></div>
           <div style={{margin:'10px 4px',width:'90%',height:'35%'}}><SkeletonLoader /></div>
         </div>
       </div>
       <div className='card'>
-        <div style={{width:'100%',height:'40%'}}><SkeletonLoader /> </div>
-        <div style={{width:'100%',height:'60%',padding:'4px'}}>
+        <div style={{width:'100%',height:'50%'}}><SkeletonLoader /> </div>
+        <div style={{width:'100%',height:'50%',padding:'4px'}}>
           <div style={{margin:'10px 4px',width:'90%',height:'15%'}}><SkeletonLoader /></div>
           <div style={{margin:'10px 4px',width:'90%',height:'35%'}}><SkeletonLoader /></div>
         </div>
